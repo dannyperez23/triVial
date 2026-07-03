2c38d9256459ac64f936a97080d8d39f351971af
dannyperez23 <dannyp1942@gmail.com>
2026-07-03 01:20:52 -0500

    Initial commit: Spring boot project with PostgreSQL configuration

---
+++ src/main/java/com/dannyperez23/triVial/TriVialApplication.java
@@ -0,0 +1,13 @@
+package com.dannyperez23.triVial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TriVialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TriVialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dannyperez23/triVial/TriVialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dannyperez23.triVial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TriVialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
